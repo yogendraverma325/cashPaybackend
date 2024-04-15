b4aa608094b76fc01711b9308b53839c24b5efc2
Manish Maurya <manishmaurya@teamcomputers.com>
2024-04-15 17:26:43 +0530

    Added SBU Mapping

--- src/api/v1/user/user.controller.js
+++ src/api/v1/user/user.controller.js
@@ -217,7 +217,52 @@ class UserController {
                     where: {
                         isActive: 1
                     },
-                    attributes: ['buName', 'buCode', 'parentBuId']
+                    attributes: ['buName', 'buCode']
+                }]
+            })
+
+            return respHelper(res, {
+                status: 200,
+                data: buData
+            })
+        } catch (error) {
+            console.log(error)
+            return respHelper(res, {
+                status: 500
+            })
+        }
+    }
+
+    async sbu(req, res) {
+        try {
+            const companyId = req.query.companyId
+            const buId = req.query.buId
+
+            if (!companyId) {
+                return respHelper(res, {
+                    status: 404,
+                    msg: "Company ID required"
+                })
+            }
+
+            if (!buId) {
+                return respHelper(res, {
+                    status: 404,
+                    msg: "Bu ID required"
+                })
+            }
+
+            const buData = await db.sbuMapping.findAll({
+                where: {
+                    companyId,
+                    buId
+                },
+                include: [{
+                    model: db.buMaster,
+                    where: {
+                        isActive: 1
+                    },
+                    attributes: ['buName', 'buCode']
                 }]
             })
 
